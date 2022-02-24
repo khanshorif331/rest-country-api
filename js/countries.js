613352fdf26e7d978b16d27cdc83acbb14fe6e6a
khanshorif331 <khanshorif331@gmail.com>
2022-02-25 01:17:45 +0600

    dynamically changed the value of data

--- js/countries.js
+++ js/countries.js
@@ -9,17 +9,34 @@ loadCountries()
 const displayCountries = countries=>{
     const countriesDiv = document.getElementById('countries')
     for(const country of countries){
-        console.log(country);
+        // console.log(country);
         const div = document.createElement('div')
         div.classList.add('country')
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name;
-        div.appendChild(h3);
-        const p = document.createElement('p')
-        p.innerText=country.capital
-        div.appendChild(p)
+        div.innerHTML=`
+        <h3>${country.name}</h3>
+        <p>${country.capital}</p>
+        <button onclick ="loadCountryByName('${country.name}')">Details</button>
+        `
         countriesDiv.appendChild(div)
          
     }
 }
 
+const loadCountryByName = name =>{
+    const url =`https://restcountries.com/v2/name/${name}
+    `
+    fetch(url)
+    .then(res => res.json())
+    .then(data=>displayCountryDetail(data[0]))
+}
+
+
+const displayCountryDetail = country =>{
+    console.log(country);
+    const countryDiv = document.getElementById('country-detail')
+    countryDiv.innerHTML= `
+        <h5>${country.name}</h5>
+        <p>Population:${country.population}</p>
+        <img width="300px" src="${country.flag}">
+    `
+}
